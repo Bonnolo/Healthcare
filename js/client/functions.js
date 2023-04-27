812804742596a0e58725e6db42c600ff957b6032
Bonnolo <fbonetti00@gmail.com>
2023-04-27 09:53:20 +0200

    minor fixes on html and js

--- js/client/functions.js
+++ js/client/functions.js
@@ -1,8 +1,7 @@
-
 function inserDataLastSold(typeOfClass, typeOfElement, typeOfData, data) {
-    let dataValue = Object.values(data);
-    for (const data of dataValue) {
-      //console.log(data);
+  let dataValue = Object.values(data);
+  for (const data of dataValue) {
+    //console.log(data);
     let element = document.createElement(`${typeOfElement}`);
     element.classList.add(`${typeOfClass}`);
     let idSelector = document.querySelector(`${typeOfData}`);
@@ -12,37 +11,39 @@ function inserDataLastSold(typeOfClass, typeOfElement, typeOfData, data) {
                         <p>${data.quantity}</p>
                         <p>${data.product_id}</p>
                         <p>${data.price.toFixed(2)}€</p>`;
-    }
+  }
 }
 
 function inserDataOrders(typeOfClass, typeOfElement, typeOfData, data) {
-    let dataValue = Object.values(data);
-    for (const data of dataValue) {
-        let element = document.createElement(`${typeOfElement}`);
-        element.classList.add(`${typeOfClass}`);
-        let idSelector = document.querySelector(`${typeOfData}`);
-        idSelector.after(element);
-        let date = new Date(data.date_of_order);
-        let year = date.getFullYear();
-        let month = date.getMonth() + 1;
-        let day = date.getDate();
-        element.innerHTML = `
+  let dataValue = Object.values(data);
+  for (const data of dataValue) {
+    let element = document.createElement(`${typeOfElement}`);
+    element.classList.add(`${typeOfClass}`);
+    let idSelector = document.querySelector(`${typeOfData}`);
+    idSelector.after(element);
+    let date = new Date(data.date_of_order);
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+    element.innerHTML = `
                             <p>${day}/${month}/${year}</p>
                             <p>${data.name}</p>
                             <p>${data.quantity}</p>
                             <p>${data.price}€</p>
                             <p>${data.product_id}</p>`;
-    }
+  }
 }
 
 function inserDataStock(typeOfClass, typeOfElement, typeOfData, data) {
-    let dataValue = Object.values(data);
-    let dataCount = data.length
-    let maxStock = 100
-    let percentageInStock = (dataCount / maxStock) * 100
-    document.querySelector(".orange").style.width = `${percentageInStock}%`
-    document.querySelector("#stockBar").innerHTML = `${percentageInStock.toFixed(0)}% / 100%`
-    for (const data of dataValue) {
+  let dataValue = Object.values(data);
+  let dataCount = data.length;
+  let maxStock = 100;
+  let percentageInStock = (dataCount / maxStock) * 100;
+  document.querySelector(".orange").style.width = `${percentageInStock}%`;
+  document.querySelector("#stockBar").innerHTML = `${percentageInStock.toFixed(
+    0
+  )}% / 100%`;
+  for (const data of dataValue) {
     let element = document.createElement(`${typeOfElement}`);
     element.classList.add(`${typeOfClass}`);
     let idSelector = document.querySelector(`${typeOfData}`);
@@ -58,11 +59,11 @@ function inserDataStock(typeOfClass, typeOfElement, typeOfData, data) {
                         <p>${data.buy_price.toFixed(2)}€</p>
                         <p>${data.sell_price.toFixed(2)}€</p>
                         <p>${day}/${month}/${year}</p>`;
-    }
+  }
 }
 function entrateData(typeOfClass, typeOfElement, typeOfData, data) {
-    let dataValue = Object.values(data);
-    for (const data of dataValue) {
+  let dataValue = Object.values(data);
+  for (const data of dataValue) {
     let element = document.createElement(`${typeOfElement}`);
     element.classList.add(`${typeOfClass}`);
     let idSelector = document.querySelector(`${typeOfData}`);
@@ -77,11 +78,11 @@ function entrateData(typeOfClass, typeOfElement, typeOfData, data) {
                         <p>${data.quantity}</p>
                         <p>${data.product_id}</p>
                         <p>${data.price.toFixed(2)}€</p>`;
-    }
+  }
 }
 function usciteData(typeOfClass, typeOfElement, typeOfData, data) {
-    let dataValue = Object.values(data);
-    for (const data of dataValue) {
+  let dataValue = Object.values(data);
+  for (const data of dataValue) {
     let element = document.createElement(`${typeOfElement}`);
     element.classList.add(`${typeOfClass}`);
     let idSelector = document.querySelector(`${typeOfData}`);
@@ -96,7 +97,13 @@ function usciteData(typeOfClass, typeOfElement, typeOfData, data) {
                         <p>${data.quantity}</p>
                         <p>${data.product_id}</p>
                         <p>${data.price.toFixed(2)}€</p>`;
-    }
+  }
 }
 
-    export {inserDataLastSold, inserDataStock, inserDataOrders, entrateData, usciteData}
+export {
+  inserDataLastSold,
+  inserDataStock,
+  inserDataOrders,
+  entrateData,
+  usciteData,
+};
--- js/client/main.js
+++ js/client/main.js
@@ -1,10 +1,15 @@
-import {inserDataLastSold, inserDataStock, inserDataOrders, entrateData, usciteData} from "./functions.js";
+import {
+  inserDataLastSold,
+  inserDataStock,
+  inserDataOrders,
+  entrateData,
+  usciteData,
+} from "./functions.js";
 
 let stock = "http://127.0.0.1:3000/stock";
 let itemsSold = "http://127.0.0.1:3000/solds";
 let orders = "http://127.0.0.1:3000/orders";
-// Get's dbtable on http://127.0.0.1:3000/stock
-
+// Get's dbtable on http://127.0.0.1:3000/
 
 const fetchdata = async (url) => {
   try {
@@ -25,16 +30,14 @@ async function fillStock(dbtable) {
 async function fillLastSold(dbtable) {
   let data = await dbtable;
   inserDataLastSold("data", "div", "#dataSold", data);
-  entrateData("dataEntrate","div", "#entrateData",data);
+  entrateData("dataEntrate", "div", "#entrateData", data);
 }
 
 async function fillOrders(dbtable) {
   let data = await dbtable;
   inserDataOrders("dataEntrate", "div", "#dataOrders", data);
-  usciteData("dataEntrate","div", "#usciteData",data);
+  usciteData("dataEntrate", "div", "#usciteData", data);
 }
-
-
 
 // running fetch
 fillStock(fetchdata(stock));
