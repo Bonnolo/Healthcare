65e3406717f1fb28d025202faef883c2c1537df0
Bonnolo <fbonetti00@gmail.com>
2023-05-02 16:11:49 +0200

    added searchbar (in fix)

--- js/client/main.js
+++ js/client/main.js
@@ -43,3 +43,33 @@ async function fillOrders(dbtable) {
 fillStock(fetchdata(stock));
 fillLastSold(fetchdata(itemsSold));
 fillOrders(fetchdata(orders));
+// searchbar
+
+// get search bar element
+const searchInput = document.querySelector("#searchInput");
+
+// store name elements in array-like object
+const namesFromDOM = document.getElementsByClassName("dataStock");
+
+// listen for user events
+searchInput.addEventListener("keyup", (event) => {
+    const { value } = event.target;
+    
+    // get user search input converted to lowercase
+    const searchQuery = value.toLowerCase();
+    
+    for (const nameElement of namesFromDOM) {
+        // store name text and convert to lowercase
+        let name = nameElement.textContent.toLowerCase();
+        
+        // compare current name to search input
+        if (name.includes(searchQuery)) {
+            // found name matching search, display it
+            nameElement.style.display = "grid";
+            
+        } else {
+            // no match, don't display name
+            nameElement.style.display = "none";
+        }
+    }
+});
